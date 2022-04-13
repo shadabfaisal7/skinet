52f110d0eab12c434c565eec20fd2d258a48ad2b
shadabfaisal7 <shadabfaisal710@gmail.com>
2022-04-14 00:29:44 +0530

    End of section 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using Infrastructure.Data;
 using API.Helpers;
 using API.Middleware;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -22,6 +23,13 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_config
+                    .GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
